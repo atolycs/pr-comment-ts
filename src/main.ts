7ddbbfa6d335aea0bc883a51684351c4861e0736
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-08 13:57:40 +0900

    feat: update codes

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,25 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { getOctokit, context } from '@actions/github'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
+  core.debug('Get Octokit')
+  core.info(`==> Add comment to PR #${context.payload.pull_request?.number}`)
+  const token = core.getInput('token')
+  const message = core.getInput('message')
+  const octokit = getOctokit(token)
+  const pull_number = context.payload.pull_request?.number
+
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    await octokit.rest.pulls.createReview({
+      ...context.repo,
+      // @eslint-disable
+      // @ts-ignore
+      pull_number,
+      // @eslint-enable
+      event: 'COMMENT',
+      body: message
+    })
+  } catch (err: any) {
+    core.setFailed(`==> ${err.message}`)
   }
 }
